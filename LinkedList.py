13a1ff71d3cc3a49075d5521808acf97209e42fb
remo <zyduremik@gmail.com>
2025-11-05 18:05:19 +0100

    Find kth node from end of linked list

--- LinkedList.py
+++ LinkedList.py
@@ -24,6 +24,11 @@ class LinkedList:
         
 
     def find_middle_node(self):
+        """
+        Funtion finds middle node of linked list by using two pointer approach.
+        One pointer moves slow and second pointer moves fast.
+        When fast pointer reaches end of linked list our slow pointer is at middle.
+        """
         temp = self.head
         forward =self.head
         if self.head is None:
@@ -32,6 +37,26 @@ class LinkedList:
             temp = temp.next
             forward = forward.next.next
         return temp
+    
+def find_kth_from_end(ll, k):    
+    """
+    Function to find kth node from end of linked list.
+    It uses two pointer approach also. We takes that 1 from end is same as last one.
+    First we move fast pointer to k node. Then we move both slow pointer and fast pointer until end.
+    When fast pointer reaches end our slow pointer is at kth node from end."""   
+    slow = ll.head
+    fast = ll.head
+        
+    for i in range(k):
+        if fast is None:
+            return None
+        fast = fast.next
+        
+    while fast:
+        slow = slow.next
+        fast = fast.next
+            
+    return slow
 
 my_linked_list = LinkedList(1)
 my_linked_list.append(2)
@@ -39,7 +64,14 @@ my_linked_list.append(3)
 my_linked_list.append(4)
 my_linked_list.append(5)
 
-print( my_linked_list.find_middle_node().value )
+#print( my_linked_list.find_middle_node().value )
+
+k = 2
+result = find_kth_from_end(my_linked_list, k)
+
+print(result.value)
+
+
 
 
 
